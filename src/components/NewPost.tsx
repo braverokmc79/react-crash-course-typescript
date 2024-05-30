7ee11520daa6d796e4f0678df3c3e8241d7342b9
braverokmc79 <braverokmc79@gmail.com>
2024-05-30 16:46:36 +0900

    587.상태 올리기

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,10 +1,13 @@
 import classes from './NewPost.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewPost: React.FC = () => {
 
+  const [enteredBody, setEnteredBody] = useState<string>("");
+  
   const changeBodyHandler: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
     console.log('changeBodyHandler:', event.target.value);
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -13,6 +16,8 @@ const NewPost: React.FC = () => {
         <label htmlFor="body">내용</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
+
       <p>
         <label htmlFor="name">작성자</label>
         <input type="text" id="name" required />
