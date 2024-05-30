1ecf3432d66c6bf8624ff5db85fa3310edcad76b
braverokmc79 <braverokmc79@gmail.com>
2024-05-30 17:12:18 +0900

    587.상태 올리기

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,26 +1,23 @@
 import classes from './NewPost.module.css';
-import React, { useState } from 'react';
 
-const NewPost: React.FC = () => {
+interface NewPostProps{
+  onBodyChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+  onAuthorChange: React.ChangeEventHandler<HTMLInputElement>;
+}
 
-  const [enteredBody, setEnteredBody] = useState<string>("");
-  
-  const changeBodyHandler: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-    console.log('changeBodyHandler:', event.target.value);
-    setEnteredBody(event.target.value);
-  }
+const NewPost: React.FC<NewPostProps> = (prpos) => {
 
   return (
     <form className={classes.form} >
       <p>
         <label htmlFor="body">내용</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={prpos.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
+      {/* <p>{enteredBody}</p> */}
 
       <p>
         <label htmlFor="name">작성자</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={prpos.onAuthorChange}/>
       </p>
     </form>
   );
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,17 +1,29 @@
-import React from "react";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 import NewPost from "./NewPost";
+import React, { useState } from 'react';
 
 
 const PostsList: React.FC = () => {
+
+  const [enteredBody, setEnteredBody] = useState<string>("");
+  const [enteredAuthor, setEnteredAuthor] = useState<string>("");
+  
+  const bodyChangeHandler: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {   
+    setEnteredBody(event.target.value);
+  }
+
+  const authorChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {    
+    setEnteredAuthor(event.target.value);
+  }
+
+
   return (
     <>
-    <NewPost />
+    <NewPost  onBodyChange={bodyChangeHandler}   onAuthorChange={authorChangeHandler}   />
+
     <ul className={classes.posts}>
-      <Post author="Max" body="React.js is awesome!" />
-      <Post author="Manuel" body="Chec out the full course!" />
-      <Post author="Manuel" body="Chec out the full course!" />
+      <Post author={enteredAuthor} body={enteredBody} />
       <Post author="Manuel" body="Chec out the full course!" />
     </ul>
     </>
