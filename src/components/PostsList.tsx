ce7c265ff3b189ca9d5d5261d92a234d1206cdfc
braverokmc79 <braverokmc79@gmail.com>
2024-06-01 18:12:22 +0900

    로딩 상태 관리

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -19,18 +19,24 @@ export interface PostData {
 const PostsList: React.FC<PostListProps> = (props) => {
  
   const [posts, setPosts] = useState<PostData[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setIsFetching(true);
       try {
         const response = await fetch(API_BASE_URL + "/posts");
-        if (!response.ok) throw new Error("Failed to fetch posts.");}
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts.");
+        }
         const data = await response.json();
         setPosts(data.posts);
       } catch (error) {
         console.error("게시글을 가져오지 못했습니다. ",error);
       }
+
+      setIsFetching(false);
     };
 
     fetchPosts();
@@ -62,16 +68,22 @@ const PostsList: React.FC<PostListProps> = (props) => {
     <>
       {modalContent}
 
-       {posts.length>0 &&
+       {!isFetching && posts.length>0 &&
           <ul className={classes.posts}>
             {posts.map((post, index) => (<Post key={index} author={post.author} body={post.body} />))}
           </ul>
         }
-        {posts.length ===0 && 
+        {!isFetching && posts.length ===0 && 
           <div style={{textAlign:"center", color:'white'}}>
               <h2>게시글이 없습니다.</h2>
               <p>포스트를 작성해 주세요!</p>
           </div>
+        }
+
+        {isFetching&&
+         <div style={{textAlign:"center", color:'white'}}>
+            <h2>데이터 가져오는 중...</h2>            
+         </div>
         }
     </>
   );
